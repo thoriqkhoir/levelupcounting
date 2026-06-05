4a92cc41a5a572a19e05a8540bad4649dacc8530
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-06-05 08:49:33 +0700

    update build

--- public/build/assets/icon-CrjNXVyq.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DFyoZe8z.js";import{c as t}from"./utils-DGmS6-P9.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
--- public/build/assets/skeleton-BcphBaWG.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-DFyoZe8z.js";import{c as r}from"./utils-DGmS6-P9.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
