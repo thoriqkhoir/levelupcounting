d0d4f71e7c721d44bcfef5cd996d14efb6580b45
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-06-12 10:42:21 +0700

    build

--- public/build/assets/icon-DgVC30mo.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CUAKmMY0.js";import{c as t}from"./utils-C0BTS9ZT.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
