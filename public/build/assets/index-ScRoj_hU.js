ddb40b6ff2c8c4a827af8d6831ee44a6e6360866
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-07-11 15:31:00 +0700

    build

--- public/build/assets/index-ScRoj_hU.js
+++
@@ -1,1 +0,0 @@
-import{g as r,a}from"./app-Cw6tD_Xt.js";var t=a();const e=r(t);export{e as R,t as r};
--- public/build/assets/presentation-B129MJGi.js
+++
@@ -1,6 +0,0 @@
-import{c as e}from"./createLucideIcon-CKsFtw0X.js";/**
- * @license lucide-react v0.475.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["path",{d:"M2 3h20",key:"91anmk"}],["path",{d:"M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3",key:"2k9sn8"}],["path",{d:"m7 21 5-5 5 5",key:"bip4we"}]],n=e("Presentation",t);export{n as P};
