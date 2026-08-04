fbc16136b5a132f2db462aeab92b2f6e4f37cf9f
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-08-04 10:00:29 +0700

    update build

---
+++ public/build/assets/icon-BDOC01Jm.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Dz_Wk9yZ.js";import{c as t}from"./utils-DkPmKwox.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
