f3fc3a22f1451bc5e9a7969adb4e93c2b219d62d
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-08-12 11:34:17 +0700

    update build

---
+++ public/build/assets/skeleton-BsE66KA9.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-BuAv9-rW.js";import{c as r}from"./utils-mcsN1eSd.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
