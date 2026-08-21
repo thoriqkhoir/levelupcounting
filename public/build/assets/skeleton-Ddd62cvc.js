9568e0188b205925627150c48bc99ffc6c8cfe36
alifzaidan <zaidanalif22710@gmail.com>
2026-08-21 13:10:10 +0700

    update build

---
+++ public/build/assets/skeleton-Ddd62cvc.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-DqNS862-.js";import{c as r}from"./utils-BoeU8uwC.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
