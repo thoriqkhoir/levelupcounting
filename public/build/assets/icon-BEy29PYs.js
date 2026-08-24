dbeda87dd4cbd089648c50c10bbd70a66c9bd5cd
alifzaidan <zaidanalif22710@gmail.com>
2026-08-24 18:00:16 +0700

    update build

--- public/build/assets/icon-BEy29PYs.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-VTh682UX.js";import{c as t}from"./utils-B5T9tRaH.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
